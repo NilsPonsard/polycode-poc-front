c0ec8aea3676455df4eb1922206c38af1419073f
Nils Ponsard <nilsponsard@gmail.com>
2022-05-04 19:18:03 +0200

    feat(editor): add markdown and toolbar

--- pages/editor/[exerciseId].tsx
+++ pages/editor/[exerciseId].tsx
@@ -1,25 +1,63 @@
 import { useRouter } from 'next/router';
 import styles from '../../styles/Editor.module.css';
 import Editor from '@monaco-editor/react';
+import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from '@mui/material';
+
+const languages = ['typescript', 'javascript', 'python', 'java', 'rust'];
 
 export default function EditorPage() {
   const router = useRouter();
 
   const exerciseId = router.query.exerciseId;
+  const [markdown, setMarkdown] = useState(`# ${exerciseId}`);
+  const [code, setCode] = useState(`console.log('Exercise ${exerciseId}');`);
+  const [language, setLanguage] = useState('typescript');
+
+  function handleLanguageChange(event: SelectChangeEvent<string>) {
+    setLanguage(event.target.value);
+  }
 
   return (
     <div className={styles.container}>
-      <div className={styles.module}>
-        <h2>Markdown</h2>
+      <div className={styles.toolbar}>
+        <FormControl size="small" variant="standard" sx={{width:"10em"}}>
+          <InputLabel id="language-select-label">Language</InputLabel>
+          <Select
+            labelId="language-select-label"
+            id="language-select"
+            value={language}
+            label="Age"
+            onChange={handleLanguageChange}
+          >
+            {languages.map((language) => (
+              <MenuItem key={language} value={language}>
+                {language}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      <div className={styles.markdown}>
+        <ReactMarkdown>{code}</ReactMarkdown>
       </div>
 
-      <div className={styles.module}>
+      <div className={styles.editor}>
         <Editor
-          defaultLanguage="javascript"
+          language={language}
           defaultValue="console.log('hello world !');"
+          value={code}
           height="100%"
           width="100%"
           theme="vs-dark"
+          onChange={(newValue) => setCode(newValue ?? '')}
           onValidate={(markers) => {
             console.log(markers);
           }}
