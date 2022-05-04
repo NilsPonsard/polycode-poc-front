5ebb6f7a7035e12e84c0fb973376d860553b5237
Nils Ponsard <nilsponsard@gmail.com>
2022-05-04 13:52:26 +0200

    feat(ui): add app bar

---
+++ components/GeneralAppBar.tsx
@@ -0,0 +1,116 @@
+import AddIcon from '@mui/icons-material/Add';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  SxProps,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import Link from 'next/link';
+import router from 'next/router';
+import { useState } from 'react';
+
+const pageLinksx: SxProps = {
+  paddingLeft: '2rem',
+};
+
+export function GeneralAppBar() {
+  const user = {
+    username: 'test',
+    id: 'test',
+  };
+
+  function handleLogout() {
+    console.error('unimplemented !');
+  }
+
+  const [userMenuAnchor, setUserMenuAnchor] = useState<HTMLElement | null>(
+    null,
+  );
+
+  const handleUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setUserMenuAnchor(event.currentTarget);
+  };
+  const handleUserMenuClose = () => {
+    setUserMenuAnchor(null);
+  };
+
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          <Link href={'/'}>PolyCode</Link>
+        </Typography>
+
+        <Typography variant="h6" component="span" sx={pageLinksx}>
+          <Link href={'/'}>Practice</Link>
+        </Typography>
+
+        <Typography variant="h6" component="span" sx={pageLinksx}>
+          <Link href={'/'}>Certification</Link>
+        </Typography>
+
+        <Typography variant="h6" component="span" sx={pageLinksx}>
+          <Link href={'/'}>Challenges</Link>
+        </Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        {user === undefined && <div>loading</div>}
+        {user ? (
+          <Button
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleUserMenu}
+            color="inherit"
+            sx={{ textTransform: 'none' }}
+            endIcon={<ArrowDropDownIcon />}
+          >
+            {user.username}
+          </Button>
+        ) : (
+          <Link href="/login" passHref>
+            <Button color="inherit">Login</Button>
+          </Link>
+        )}
+        <Menu
+          id="user-menu"
+          anchorEl={userMenuAnchor}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          open={Boolean(userMenuAnchor)}
+          onClose={handleUserMenuClose}
+        >
+          <MenuItem
+            onClick={() => {
+              setUserMenuAnchor(null);
+              router.push(`/users/${user.id}`);
+            }}
+          >
+            Profile
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              setUserMenuAnchor(null);
+              router.push(`/password`);
+            }}
+          >
+            Change password
+          </MenuItem>
+          <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { GeneralAppBar } from '../components/GeneralAppBar';
+
+const DOTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#821980',
+    },
+    secondary: {
+      main: '#EDAE49',
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={DOTheme}>
+      <main>
+      <GeneralAppBar />
+        <Component {...pageProps} />
+      </main>
+      <footer>
+        footer
+      </footer>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
